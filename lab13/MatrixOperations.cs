0c53a3a43f87d4fbdd0c8c16b0e49f72a2f2cec0
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2024-03-03 20:15:57 +0200

    Add project files.

---
+++ lab13/MatrixOperations.cs
@@ -0,0 +1,78 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+namespace lab13
+{
+    public class MatrixOperations
+    {
+        public static double[,] GenerateMatrix(int rows, int columns)
+        {
+            double[,] matrix = new double[rows, columns];
+            Random rand = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = rand.NextDouble() * 10;
+                }
+            }
+
+            return matrix;
+        }
+
+        public static double[,] MultiplyMatricesBlock(double[,] A, double[,] B, int blockSize)
+        {
+            var matrixA = Matrix<double>.Build.DenseOfArray(A);
+            var matrixB = Matrix<double>.Build.DenseOfArray(B);
+            int rowsA = matrixA.RowCount;
+            int columnsB = matrixB.ColumnCount;
+            double[,] result = new double[rowsA, columnsB];
+
+            for (int i = 0; i < rowsA; i += blockSize)
+            {
+                for (int j = 0; j < columnsB; j += blockSize)
+                {
+                    for (int k = 0; k < matrixA.ColumnCount; k += blockSize)
+                    {
+                        int blockRows = Math.Min(blockSize, rowsA - i);
+                        int blockColumns = Math.Min(blockSize, columnsB - j);
+                        int blockK = Math.Min(blockSize, matrixA.ColumnCount - k);
+
+                        // Extract blocks
+                        var blockA = matrixA.SubMatrix(i, blockRows, k, blockK);
+                        var blockB = matrixB.SubMatrix(k, blockK, j, blockColumns);
+
+                        // Multiply blocks
+                        var blockResult = blockA.Multiply(blockB);
+
+                        // Add to result
+                        for (int ii = 0; ii < blockResult.RowCount; ii++)
+                        {
+                            for (int jj = 0; jj < blockResult.ColumnCount; jj++)
+                            {
+                                result[i + ii, j + jj] += blockResult[ii, jj];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static void PrintMatrix(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j].ToString("F3") + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ lab13/Program.cs
@@ -0,0 +1,39 @@
+﻿using lab13;
+using System.Diagnostics;
+using System.Text;
+
+Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+
+int[] mValues = { 2, 4, 6, 8, 10 }; // Число рядків матриці А
+int[] nValues = { 2, 4, 6, 8, 10 }; // Число рядків і стовбців матриці B
+int[] lValues = { 2, 4, 6, 8, 10 }; // Число стовбців матриці B
+int blockSize = 2; // Розмір блоку
+
+foreach (int m in mValues)
+{
+    foreach (int l in lValues)
+    {
+        Console.WriteLine($"Проводимо експеримент для m={m}, l={l}:");
+        Console.WriteLine("----------------------------------------");
+
+        double[,] A = MatrixOperations.GenerateMatrix(m, nValues[0]);
+        double[,] B = MatrixOperations.GenerateMatrix(nValues[0], l);
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        double[,] C = MatrixOperations.MultiplyMatricesBlock(A, B, blockSize);
+        stopwatch.Stop();
+
+        Console.WriteLine($"Час виконання: {stopwatch.Elapsed.TotalMilliseconds} мілісекунд(и)\n");
+
+        Console.WriteLine("Матриця A:");
+        MatrixOperations.PrintMatrix(A);
+
+        Console.WriteLine("\nМатриця B:");
+        MatrixOperations.PrintMatrix(B);
+
+        Console.WriteLine("\nМатриця C:");
+        MatrixOperations.PrintMatrix(C);
+
+        Console.WriteLine("\n\n");
+    }
+}
